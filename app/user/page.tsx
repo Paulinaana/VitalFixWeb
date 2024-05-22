87c646375e0f6c2dc8e738457db95f95e287dd04
Paulinaana <azuaje43@gmail.com>
2024-05-22 01:20:25 -0400

    acomodar perfil y recuperar contraseña

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -7,31 +7,31 @@ function user(){
 
 <div className="bg-white w-full flex flex-col gap-5 px-3 md:px-16 lg:px-28 md:flex-row text-[#161931]">
     <aside className="hidden py-4 md:w-1/3 lg:w-1/4 md:block">
-        <div className="sticky flex flex-col gap-2 p-4 text-sm border-r border-indigo-100 top-12">
+        <div className="sticky flex flex-col gap-2 p-20 text-sm border-r border-indigo-100 top-12">
 
-            <h2 className="pl-3 mb-4 text-2xl font-semibold">Settings</h2>
+            <h2 className="pl-3 mb-4 text-2xl font-semibold">Configuraciones</h2>
 
             <a href="#" className="flex items-center px-3 py-2.5 font-bold bg-white  text-indigo-900 border rounded-full">
-                Pubic Profile
+                Perfil
             </a>
             <a href="#"
                 className="flex items-center px-3 py-2.5 font-semibold  hover:text-indigo-900 hover:border hover:rounded-full">
-                Account Settings
+                Servicios
             </a>
             <a href="#"
                 className="flex items-center px-3 py-2.5 font-semibold hover:text-indigo-900 hover:border hover:rounded-full  ">
-                Notifications
+                Reclamos
             </a>
-            <a href="#"
+            {/* <a href="#"
                 className="flex items-center px-3 py-2.5 font-semibold hover:text-indigo-900 hover:border hover:rounded-full  ">
                 PRO Account
-            </a>
+            </a> */}
         </div>
     </aside>
     <main className="w-full min-h-screen py-1 md:w-2/3 lg:w-3/4">
         <div className="p-2 md:p-4">
             <div className="w-full px-6 pb-8 mt-8 sm:max-w-xl sm:rounded-lg">
-                <h2 className="pl-6 text-2xl font-bold sm:text-xl">Public Profile</h2>
+                <h2 className="pl-6 text-2xl font-bold sm:text-xl">Perfil</h2>
 
                 <div className="grid max-w-2xl mx-auto mt-8">
                     <div className="flex flex-col items-center space-y-5 sm:flex-row sm:space-y-0">
@@ -54,70 +54,137 @@ function user(){
                         <div className="flex flex-col space-y-5 sm:ml-8">
                             <button type="button"
                                 className="py-3.5 px-7 text-base font-medium text-indigo-100 focus:outline-none bg-[#202142] rounded-lg border border-indigo-200 hover:bg-indigo-900 focus:z-10 focus:ring-4 focus:ring-indigo-200 ">
-                                Change picture
+                                Cambiar foto
                             </button>
                             <button type="button"
                                 className="py-3.5 px-7 text-base font-medium text-indigo-900 focus:outline-none bg-white rounded-lg border border-indigo-200 hover:bg-indigo-100 hover:text-[#202142] focus:z-10 focus:ring-4 focus:ring-indigo-200 ">
-                                Delete picture
+                                Eliminar foto
                             </button>
                         </div>
                     </div>
 
+
+...
                     <div className="items-center mt-8 sm:mt-14 text-[#202142]">
+                    <h2 className="text-base font-semibold leading-7 text-gray-900">Información Personal</h2>
 
                         <div
                             className="flex flex-col items-center w-full mb-2 space-x-0 space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0 sm:mb-6">
                             <div className="w-full">
-                                <label htmlFor="first_name"
-                                    className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white">Your
-                                    first name</label>
-                                <input type="text" id="first_name"
+                                <label htmlFor="names"
+                                    className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white">
+                                    Nombres</label>
+                                <input type="text" id="names"
                                     className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
-                                    placeholder="Your first name" value="Jane" required/>
+                                    placeholder="Nombres" required/>
                             </div>
 
                             <div className="w-full">
                                 <label htmlFor="last_name"
-                                    className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white">Your
-                                    last name</label>
+                                    className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white">
+                                    Apellidos</label>
                                 <input type="text" id="last_name"
                                     className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
-                                    placeholder="Your last name" value="Ferguson" required/>
+                                    placeholder="Apellidos" required/>
                             </div>
 
                         </div>
 
                         <div className="mb-2 sm:mb-6">
-                            <label htmlFor="email"
-                                className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white">Your
-                                email</label>
-                            <input type="email" id="email"
+                            <label htmlFor="user"
+                                className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white">Usuario</label>
+                            <input type="text" id="user"
                                 className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
-                                placeholder="your.email@mail.com" required />
+                                placeholder="Nombre de usuario" required/>
                         </div>
 
                         <div className="mb-2 sm:mb-6">
-                            <label htmlFor="profession"
-                                className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white">Profession</label>
-                            <input type="text" id="profession"
+                            <label htmlFor="email"
+                                className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white">Tu
+                                correo</label>
+                            <input type="email" id="email"
                                 className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
-                                placeholder="your profession" required/>
+                                placeholder="tu.correo@mail.com" required />
                         </div>
 
-                        <div className="mb-6">
+
+                        {/* <div className="mb-6">
                             <label htmlFor="message"
                                 className="block mb-2 text-sm font-medium text-indigo-900 dark:text-white">Bio</label>
                             <textarea id="message"
                                 className="block p-2.5 w-full text-sm text-indigo-900 bg-indigo-50 rounded-lg border border-indigo-300 focus:ring-indigo-500 focus:border-indigo-500 "
                                 placeholder="Write your bio here..."></textarea>
-                        </div>
+                        </div> */}
 
                         <div className="flex justify-end">
                             <button type="submit"
-                                className="text-white bg-indigo-700  hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">Save</button>
+                                className="text-white bg-indigo-700  hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">Guardar</button>
                         </div>
 
                     </div>
+
+                    <div className="border-b border-gray-900/10 pb-12 w-auto">
+                    <h2 className="text-base font-semibold leading-7 text-gray-900">Información Personal</h2>
+                    <p className="mt-1 text-sm leading-6 text-gray-600">Utilice una dirección permanente en la que pueda recibir el servicio.</p>
+
+                    <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+
+                        <div className="sm:col-span-3">
+                        <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">Teléfono</label>
+                        <div className="mt-2">
+                            <input type="text" name="last-name" id="last-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                        </div>
+                        </div>
+
+                        <div className="sm:col-span-3">
+                        <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">País</label>
+                        <div className="mt-2">
+                            <select id="country" name="country" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+                            <option>Venezuela</option>
+                            <option>Canada</option>
+                            <option>Mexico</option>
+                            </select>
+                        </div>
+                        </div>
+
+                        <div className="col-span-full">
+                        <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">Dirección</label>
+                        <div className="mt-2">
+                            <input type="text" name="street-address" id="street-address" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                        </div>
+                        </div>
+
+                        <div className="col-span-full">
+                        <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">Referencias</label>
+                        <div className="mt-2">
+                            <input type="text" name="street-address" id="street-address" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                        </div>
+                        </div>
+
+                        <div className="sm:col-span-2 sm:col-start-1">
+                        <label htmlFor="city" className="block text-sm font-medium leading-6 text-gray-900">Ciudad</label>
+                        <div className="mt-2">
+                            <input type="text" name="city" id="city"  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                        </div>
+                        </div>
+
+                        <div className="sm:col-span-2">
+                        <label htmlFor="region" className="block text-sm font-medium leading-6 text-gray-900">Estado</label>
+                        <div className="mt-2">
+                            <input type="text" name="region" id="region" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                        </div>
+                        </div>
+
+                        <div className="sm:col-span-2">
+                        <label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-gray-900">ZIP / Codigo Postal</label>
+                        <div className="mt-2">
+                            <input type="text" name="postal-code" id="postal-code"  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6" />
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+
+
                 </div>
             </div>
         </div>
