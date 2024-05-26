ba6a6becbc313d0aa780676a430d8181a2af6548
Paulinaana <azuaje43@gmail.com>
2024-05-25 21:25:18 -0400

    errores

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -7,7 +7,7 @@ function user(){
 
 <div className="bg-white w-full flex flex-col gap-5 px-3 md:px-16 lg:px-28 md:flex-row text-[#161931]">
     <aside className="hidden py-4 md:w-1/3 lg:w-1/4 md:block">
-        <div className="sticky flex flex-col gap-2 p-20 text-sm border-r border-indigo-100 top-12">
+        <div className="mt-10 flex flex-col gap-2 p-20 text-sm border-r border-indigo-100 top-12">
 
             <h2 className="pl-3 mb-4 text-2xl font-semibold">Configuraciones</h2>
 
