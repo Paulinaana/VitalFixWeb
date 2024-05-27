98879db4f91212485e780645c198bb3594059dd9
Paulinaana <azuaje43@gmail.com>
2024-05-26 22:18:12 -0400

    arreglos

--- app/serviceInfo/page.tsx
+++ app/serviceInfo/page.tsx
@@ -302,10 +302,20 @@ function serviceInfo(){
             <dd className="font-medium text-gray-900"><time dateTime="2021-03-22">Mayo 22, 2024</time></dd>
         </dl>
         <div className="mt-4 sm:mt-0">
-            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+            {/* <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
             View invoice
             <span aria-hidden="true"> &rarr;</span>
-            </a>
+            </a> */}
+
+            <div>
+            <span className="py-1 px-2 inline-flex text-sm items-center gap-x-1 font-medium bg-teal-100 text-teal-800 rounded-full dark:bg-teal-500/10 dark:text-teal-500">
+                <svg className="flex-shrink-0 size-3" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"></path>
+                <path d="m9 12 2 2 4-4"></path>
+                </svg>
+                Connected
+            </span>
+            </div>
         </div>
         </div>
 
@@ -350,12 +360,14 @@ function serviceInfo(){
                     <dd className="mt-3 space-y-3 text-gray-500">
                     <p>fake@example.com</p>
                     <p>1245346540</p>
-                    <button type="button" className="font-medium text-indigo-600 hover:text-indigo-500">Edit</button>
                     </dd>
                 </div>
                 </dl>
-                <p className="mt-6 font-medium text-gray-900 md:mt-10">Procesado el <time dateTime="2024-05-24">Mayo 24, 2024</time></p>
-                <div className="mt-6">
+                <div className="flex items-center justify-between pt-4">
+                <dt className="font-medium text-gray-900">Total de la orden</dt>
+                <dd className="font-medium text-indigo-600">$46.75</dd>
+                </div>                
+                {/* <div className="mt-6">
                 <div className="overflow-hidden rounded-full bg-gray-200">
                     <div className="h-2 rounded-full bg-indigo-600"></div>
                 </div>
@@ -365,7 +377,7 @@ function serviceInfo(){
                     <div className="text-center">Shipped</div>
                     <div className="text-right">Delivered</div>
                 </div>
-                </div>
+                </div> */}
             </div>
             </div>
 
@@ -374,53 +386,49 @@ function serviceInfo(){
         </section>
 
         {/* <!-- Billing --> */}
-        <section aria-labelledby="summary-heading" className="mt-24">
-        <h2 id="summary-heading" className="sr-only">Billing Summary</h2>
+        <section aria-labelledby="summary-heading" className="mt-10">
+        {/* <h2 id="summary-heading" className="sr-only">Billing Summary</h2> */}
 
         <div className="rounded-lg bg-gray-50 px-6 py-6 lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-0 lg:py-8">
             <dl className="grid grid-cols-1 gap-6 text-sm sm:grid-cols-2 md:gap-x-8 lg:col-span-5 lg:pl-8">
             <div>
-                <dt className="font-medium text-gray-900">Billing address</dt>
+                <dt className="font-medium text-gray-900">Garantia</dt>
                 <dd className="mt-3 text-gray-500">
-                <span className="block">Floyd Miles</span>
-                <span className="block">7363 Cynthia Pass</span>
-                <span className="block">Toronto, ON N3Y 4H8</span>
+                <span className="block">6 meses</span>
+                <span className="block">25 de Octubre de 2024</span>
+                <span className="py-1 px-2 inline-flex items-center gap-x-1 text-xs bg-gray-300 text-gray-800 rounded-full">
+                        <svg className="flex-shrink-0 size-3" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M18.36 6.64a9 9 0 1 1-12.73 0"></path>
+                        <line x1="12" x2="12" y1="2" y2="12"></line>
+                        </svg>
+                        Sin garantia
+                </span>
                 </dd>
             </div>
             <div>
-                <dt className="font-medium text-gray-900">Payment information</dt>
+                <dt className="font-medium text-gray-900">Informacion de Pago</dt>
                 <dd className="mt-3 flex">
                 <div>
-                    <svg aria-hidden="true" width="36" height="24" viewBox="0 0 36 24" className="h-6 w-auto">
+                    {/* <svg aria-hidden="true" width="36" height="24" viewBox="0 0 36 24" className="h-6 w-auto">
                     <rect width="36" height="24" rx="4" fill="#224DBA" />
                     <path d="M10.925 15.673H8.874l-1.538-6c-.073-.276-.228-.52-.456-.635A6.575 6.575 0 005 8.403v-.231h3.304c.456 0 .798.347.855.75l.798 4.328 2.05-5.078h1.994l-3.076 7.5zm4.216 0h-1.937L14.8 8.172h1.937l-1.595 7.5zm4.101-5.422c.057-.404.399-.635.798-.635a3.54 3.54 0 011.88.346l.342-1.615A4.808 4.808 0 0020.496 8c-1.88 0-3.248 1.039-3.248 2.481 0 1.097.969 1.673 1.653 2.02.74.346 1.025.577.968.923 0 .519-.57.75-1.139.75a4.795 4.795 0 01-1.994-.462l-.342 1.616a5.48 5.48 0 002.108.404c2.108.057 3.418-.981 3.418-2.539 0-1.962-2.678-2.077-2.678-2.942zm9.457 5.422L27.16 8.172h-1.652a.858.858 0 00-.798.577l-2.848 6.924h1.994l.398-1.096h2.45l.228 1.096h1.766zm-2.905-5.482l.57 2.827h-1.596l1.026-2.827z" fill="#fff" />
                     </svg>
-                    <p className="sr-only">Visa</p>
+                    <p className="sr-only">Visa</p> */}
                 </div>
                 <div className="ml-4">
-                    <p className="text-gray-900">Ending with 4242</p>
-                    <p className="text-gray-600">Expires 02 / 24</p>
+                    <p className="text-gray-600">Tipo de Pago:</p>
+                    <p className="text-gray-600">34467577</p>
+                    
                 </div>
                 </dd>
             </div>
             </dl>
 
             <dl className="mt-8 divide-y divide-gray-200 text-sm lg:col-span-7 lg:mt-0 lg:pr-8">
-            <div className="flex items-center justify-between pb-4">
-                <dt className="text-gray-600">Subtotal</dt>
-                <dd className="font-medium text-gray-900">$72</dd>
-            </div>
-            <div className="flex items-center justify-between py-4">
-                <dt className="text-gray-600">Shipping</dt>
-                <dd className="font-medium text-gray-900">$5</dd>
-            </div>
-            <div className="flex items-center justify-between py-4">
-                <dt className="text-gray-600">Tax</dt>
-                <dd className="font-medium text-gray-900">$6.16</dd>
-            </div>
+            
             <div className="flex items-center justify-between pt-4">
-                <dt className="font-medium text-gray-900">Order total</dt>
-                <dd className="font-medium text-indigo-600">$83.16</dd>
+                <dt className="font-medium text-gray-900"></dt>
+                <dd className="font-medium text-indigo-600"></dd>
             </div>
             </dl>
         </div>
